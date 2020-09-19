be07b4fd3cf3c7eb0d1c6d1315534a9d9a8ff3c2
govardhan <27.govardhan@gmail.com>
2020-09-20 01:55:49 +0530

    added window open/close functionalities

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/css/css';
 import { Controlled as ControlledEditor } from 'react-codemirror2';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCompressAlt,
+  faExpandAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 function Editor(props) {
   const { displayName, onChange, language, value } = props;
+
+  const [open, setOpen] = useState(true);
 
   function handleChange(editor, data, val) {
     onChange(val);
   }
 
   return (
-    <div className='editor-container'>
+    <div className={`editor-container ${open ? '' : 'collapsed'}`}>
       <div className='editor-title'>
         {displayName}
-        <button type='button'>O/C</button>
+        <button
+          type='button'
+          className='expand-collapse-btn'
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+        >
+          <FontAwesomeIcon
+            icon={open ? faCompressAlt : faExpandAlt}
+          />
+        </button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
